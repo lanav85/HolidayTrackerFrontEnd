1efdadd94c4175ec501f4b16b21e50cb61a1860d
lanav85 <lanaviana.o@gmail.com>
2024-04-14 23:20:25 -0300

    Added Hove over to sidebar

--- Projects/HolidayTrackerFrontEnd/HolidayTrackerFrontEnd/src/components/Sidebar.jsx
+++ Projects/HolidayTrackerFrontEnd/HolidayTrackerFrontEnd/src/components/Sidebar.jsx
@@ -7,46 +7,47 @@ import {
   CDBSidebarMenuItem,
 } from 'cdbreact';
 import { NavLink } from 'react-router-dom';
+import '@/App.css'; 
 
 export default  function Sidebar() {
   return (
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-    <CDBSidebar textColor="#fff" backgroundColor="#6f42c1">
-      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+    <CDBSidebar textColor="#fff" backgroundColor="#82029b">
+      <CDBSidebarHeader>
         <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}></a>
       </CDBSidebarHeader>
 
       <CDBSidebarContent className="sidebar-content">
         <CDBSidebarMenu>
-          <NavLink exact to="/" >
-            <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
-          </NavLink>
+        <NavLink exact to="/dashboard">
+              <CDBSidebarMenuItem icon="columns" className="sidebar-menu-item" style={{ marginBottom: '30px' }}>Dashboard</CDBSidebarMenuItem>
+            </NavLink>
           
           <NavLink exact to="/profile">
-            <CDBSidebarMenuItem icon="user">Profile </CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="user" className="sidebar-menu-item" style={{ marginBottom: '30px' }}>Profile </CDBSidebarMenuItem>
           </NavLink>
 
           <NavLink exact to="/submit-request">
-            <CDBSidebarMenuItem icon="calendar-plus">Submit Request </CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="calendar-plus" className="sidebar-menu-item" style={{ marginBottom: '30px' }}>Submit Holiday Request </CDBSidebarMenuItem>
           </NavLink>
           
           <NavLink exact to="/review-requests" >
-            <CDBSidebarMenuItem icon="calendar">Review Requests </CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="calendar" className="sidebar-menu-item" style={{ marginBottom: '30px' }}>Review Holiday Requests </CDBSidebarMenuItem>
           </NavLink>
 
           <NavLink exact to="/manage-employees">
-            <CDBSidebarMenuItem icon="users">Manage Employees </CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="users" className="sidebar-menu-item">Manage Employees </CDBSidebarMenuItem>
           </NavLink>
         </CDBSidebarMenu>
       </CDBSidebarContent>
 
-      <CDBSidebarFooter style={{ textAlign: 'center' }}>
+      <CDBSidebarFooter style={{ textAlign: 'center', fontSize: '10px' }}>
         <div
           style={{
             padding: '20px 5px',
           }}
         >
-          Sidebar Footer
+          Version 1.0.0
         </div>
       </CDBSidebarFooter>
     </CDBSidebar>
