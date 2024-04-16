80c9934fbfce9b8fe49fe33d5c308dde737a152d
lanav85 <lanaviana.o@gmail.com>
2024-04-15 21:51:49 -0300

    Updated Profile

--- Projects/HolidayTrackerFrontEnd/HolidayTrackerFrontEnd/src/components/Profile.jsx
+++ Projects/HolidayTrackerFrontEnd/HolidayTrackerFrontEnd/src/components/Profile.jsx
@@ -6,110 +6,110 @@ import Row from 'react-bootstrap/Row';
 function Profile() {
   return (
     <div style={{ 
-        marginLeft: '25%', // Adjust as needed
-        marginRight: '10%', // Adjust as needed
+        marginLeft: 'calc(25% + 60px)', 
+        marginRight: '10%', 
         marginTop: '50px',
-        padding: '2vw' // Adjust as needed
+        padding: '2vw' 
       }}>
         <Form>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalName">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
           Name
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="name" placeholder="Name" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalSurname">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
         Surname
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="surname" placeholder="Surname" />
         </Col>
       </Form.Group>
       
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
           Email
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="email" placeholder="Email" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalManager">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
         Manager
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="manager" placeholder="Manager" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalRole">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
           Role
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="role" placeholder="Role" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalDepartment">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
           Department
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="department" placeholder="Department" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalHollidayAllowance">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
         Holliday Allowance
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="hollidayAllowance" placeholder="HollidayAllowance" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalStartDate">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
         Start Date
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="startDate" placeholder="Start Date" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalAddress">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
         Address
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="address" placeholder="Address" />
         </Col>
       </Form.Group>
 
       <Form.Group as={Row} className="mb-3" controlId="formHorizontalPhoneNumber">
-        <Form.Label column sm={2}>
+        <Form.Label column sm={3}>
         Phone Number
         </Form.Label>
-        <Col sm={10}>
+        <Col sm={8}>
           <Form.Control type="phoneNumber" placeholder="Phone Number" />
         </Col>
       </Form.Group>
 
 
       <Form.Group as={Row} className="mb-3">
-        <Col sm={{ span: 10, offset: 2 }}>
+        <Col sm={{ span: 20, offset: 4 }}>
         <Button  type="button" 
     color="primary" 
     class="btn btn-success btn-lg" 
-    style={{ backgroundColor: 'purple' }}>Save Changes</Button> {/* Apply btn-purple class */}
+    style={{ backgroundColor: 'purple', marginTop: '20px' }}>Save Changes</Button> 
         </Col>
       </Form.Group>
     </Form>
