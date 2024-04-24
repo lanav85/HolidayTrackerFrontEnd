22fb9f4bf7110427cfc3dff9c1c2e95ec5c3dc09
jcoll <j.collado1@universityofgalway.ie>
2024-04-24 11:12:00 +0100

    Pulling changes

--- Projects/HolidayTrackerFrontEnd/HolidayTrackerFrontEnd/src/App.jsx
+++ Projects/HolidayTrackerFrontEnd/HolidayTrackerFrontEnd/src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar.jsx";
+import Header from "./components/Header.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Sidebar />
+      <Header />
   </Router>
   )
 }
