2f7cf09e3b779547fa18a0a8d6da2a10f1b531a1
lanav85 <lanaviana.o@gmail.com>
2024-07-11 23:32:04 +0100

    Changed connection to user pool

--- src/userpool.js
+++ src/userpool.js
@@ -1,7 +1,7 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 const poolData = {
-    UserPoolId: process.env.REACT_APP_USER_POOL_ID,
-    ClientId: process.env.REACT_APP_CLIENT_ID,
+    UserPoolId: import.meta.env.VITE_REACT_APP_USER_POOL_ID,
+    ClientId: import.meta.env.VITE_REACT_APP_CLIENT_ID,
 };
 const up = new CognitoUserPool(poolData);
 export default up;
