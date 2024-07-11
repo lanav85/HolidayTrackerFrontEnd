c4c4abd2d6be14f3540258fc9df296bffeaed01e
lanav85 <lanaviana.o@gmail.com>
2024-07-11 23:25:47 +0100

    reverted to previous version

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,79 +4,8 @@ import logo from "img/logo.png";
 import background from "img/background.png";
 import "css/Login.css";
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { authenticate } from "../services/authenticate";
 
-const Login = () => {
-  const Navigate = useNavigate();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailErr, setEmailErr] = useState("");
-  const [passwordErr, setPasswordErr] = useState("");
-  const [loginErr, setLoginErr] = useState("");
-
-  const formInputChange = (formField, value) => {
-    if (formField === "email") {
-      setEmail(value);
-    }
-    if (formField === "password") {
-      setPassword(value);
-    }
-  };
-
-  const validation = () => {
-    return new Promise((resolve, reject) => {
-      if (email === "" && password === "") {
-        setEmailErr("Email is Required");
-        setPasswordErr("Password is required");
-        resolve({
-          email: "Email is Required",
-          password: "Password is required",
-        });
-      } else if (email === "") {
-        setEmailErr("Email is Required");
-        resolve({ email: "Email is Required", password: "" });
-      } else if (password === "") {
-        setPasswordErr("Password is required");
-        resolve({ email: "", password: "Password is required" });
-      } else if (password.length < 6) {
-        setPasswordErr("must be 6 character");
-        resolve({ email: "", password: "must be 6 character" });
-      } else {
-        resolve({ email: "", password: "" });
-      }
-    });
-  };
-
-  const handleClick = () => {
-    setEmailErr("");
-    setPasswordErr("");
-    validation()
-      .then(
-        (res) => {
-          if (res.email === "" && res.password === "") {
-            authenticate(email, password)
-              .then(
-                (data) => {
-                  setLoginErr("");
-                  Navigate("/dashboard");
-                },
-                (err) => {
-                  console.log(err);
-                  setLoginErr(err.message);
-                }
-              )
-              .catch((err) => console.log(err));
-          }
-        },
-        (err) => console.log(err)
-      )
-      .catch((err) => console.log(err));
-  };
-
-  /*const handleClear = () => {
+const handleClear = () => {
   setUsername("");
   setPassword("");
 };
@@ -108,7 +37,7 @@ function Login() {
     //Thats why you see when i store the user json data, i turn it into a string.
     // and then when i retrieve it, i turn the string back into json.
   }
-*/
+
   return (
     <div
       className="background"
@@ -152,6 +81,6 @@ function Login() {
       </Form>
     </div>
   );
-};
+}
 
 export default Login;
