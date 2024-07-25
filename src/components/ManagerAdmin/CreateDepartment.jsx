a73b545b07618340b30aaabb95e5f6c18f7a163a
lanav85 <lanaviana.o@gmail.com>
2024-07-25 23:44:31 +0100

    styling changes

--- src/components/ManagerAdmin/CreateDepartment.jsx
+++ src/components/ManagerAdmin/CreateDepartment.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as api from "../../api/ApiRequests";
 import "@/App.css";
 import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const CreateDepartment = () => {
   const navigate = useNavigate();
@@ -28,22 +29,37 @@ const CreateDepartment = () => {
 
   return (
     <div className="moveToRight-container">
-      <h1>Create New Department</h1>
-      <div>
-        <label>Department Name:</label>
-        <input
-          type="text"
-          value={departmentName}
-          onChange={(e) => setDepartmentName(e.target.value)}
-          required
-        />
+      <div style={{ marginTop: "50px", padding: "2vw" }}>
+        <h2
+          style={{
+            marginBottom: "25px",
+            textAlign: "center",
+          }}
+        >
+          Create New Department
+        </h2>
+        <div className="shadow p-3 mb-5 bg-white rounded">
+          <div>
+            <label style={{ padding: "20px" }}>
+              <h4>Department Name: </h4>
+            </label>
+            <input
+              type="text"
+              value={departmentName}
+              onChange={(e) => setDepartmentName(e.target.value)}
+              required
+            />
+          </div>
+          <Button
+            style={{ fontSize: "17px" }}
+            onClick={handleCreateDepartment}
+            type="submit"
+            className="btn btn-success btn-lg submit-button"
+          >
+            Create Department
+          </Button>
+        </div>
       </div>
-      <button
-        onClick={handleCreateDepartment}
-        className="btn btn-danger btn-lg delete-button"
-      >
-        Create Department
-      </button>
     </div>
   );
 };
